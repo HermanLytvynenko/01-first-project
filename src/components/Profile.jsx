f522edb39cc73116f19d911a392ee6c3727dc221
Lytvynenko Herman <lytvynenkoherman@gmail.com>
2022-05-01 18:06:00 +0300

    new folder

---
+++ src/components/Profile.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Profile = () => {
+    return <div className='content'>
+        <div>
+            <img src='https://burst.shopify.com/photos/two-customized-cars-rolling-through-a-city-street/download'
+                 title='hui' alt='ss'/>
+        </div>
+        <div>
+            <img src="https://lapkins.ru/upload/iblock/130/1309222f033fb6928ea065578276ab44.jpg" alt="Avatar"/>
+        </div>
+    </div>
+}
+
+export default Profile;
+
